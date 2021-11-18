fb88dda3c5d4df5ef3f906ffb98ab86467af2cc4
Flamcom27 <27flamcom06@gmail.com>
2021-11-18 20:57:42 +0300

    model 'Bucket' is created

--- main/models.py
+++ main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class Bucket(models.Model):
+    volume = models.IntegerField()
+    condition = models.IntegerField()
+
+    def __str__(self):
+        self.name = f'Bucket {self.volume} Liters'
+        return self.name
