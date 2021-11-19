88cf64b28af208de8c50562f598dad46c6efd548
Flamcom27 <27flamcom06@gmail.com>
2021-11-19 20:35:56 +0300

    template 'show_buckets' is created; method 'chek_condition' in model 'Bucket' is added; url path 'home/' is created

--- main/models.py
+++ main/models.py
@@ -9,3 +9,11 @@ class Bucket(models.Model):
     def __str__(self):
         self.name = f'Bucket {self.volume} Liters'
         return self.name
+
+    def check_condition(self):
+        if self.condition > self.volume:
+            self.condition = self.volume
+            return self.condition
+        elif self.condition < 0:
+            self.condition = 0
+            return self.condition
--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from .models import Bucket
+
 
 # Create your views here.
+def show_buckets(request):
+    buckets = Bucket.objects.all()
+    return render(request, "main/show_buckets.html", {"buckets": buckets})
